42a256f7d0f7bcd8875b77c9a9a4788389b59c0f
Marcos Vinicius Luiz <oi_marcos@globomail.com>
2020-12-14 17:32:02 -0300

    2-javascript

--- projeto-login/assets/js/script.js
+++ projeto-login/assets/js/script.js
@@ -3,3 +3,21 @@ const email = 'oi_marcos@globomail.com'
 console.log(email)
 console.log('O seu e-mail é: ' + email)
 console.log(`O seu e-mail é: ${email}`)
+
+document.getElementById('btn-submit').addEventListener('click', e => {
+    console.log('O botão foi clicado!')
+})
+
+document.getElementById('form-login').addEventListener('mouseenter', e => {
+   console.log('O mouse está sobre o formulário!') 
+})
+
+document.querySelector('#form-login').addEventListener('mouseleave', e => {
+    console.log('O mouse está fora do formulário!')
+})
+
+document.querySelector('#form-login').addEventListener('submit', e => {
+    e.preventDefault()
+
+    console.log('Formulário enviado!')
+})
